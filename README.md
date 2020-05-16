1d90493c411cce592467be624ff13861661ef9ab
Leonardo Moraes <leonardo.dalla@hotmail.com>
2020-05-15 21:35:16 -0300

    add opencv.js compiled js

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # visao-comp-trab1
+
+## build
+
+### build opencv.js
+    Utilizado o build do com o docker, descrito neste link:
+    https://github.com/opencv/opencv/blob/4.3.0/doc/js_tutorials/js_setup/js_setup/js_setup.markdown
+
+### baixar bibliotecas npm
+    Para baixar o restante das bibliotecas, deve-se executar:
+    
+    npm install
+
+## exemplos de implementação
+    https://docs.opencv.org/master/dc/de6/tutorial_js_nodejs.html
+    https://community.risingstack.com/opencv-tutorial-computer-vision-with-node-js/
---
+++ index.js
@@ -0,0 +1,30 @@
+const jimp = require('jimp');
+
+async function onRuntimeInitialized(){
+  // load local image file with jimp. It supports jpg, png, bmp, tiff and gif:
+  var jimpSrc = await jimp.read('./images/lena.jpg');
+  // `jimpImage.bitmap` property has the decoded ImageData that we can use to create a cv:Mat
+  var src = cv.matFromImageData(jimpSrc.bitmap);
+  // following lines is copy&paste of opencv.js dilate tutorial:
+  let dst = new cv.Mat();
+  let M = cv.Mat.ones(5, 5, cv.CV_8U);
+  let anchor = new cv.Point(-1, -1);
+  cv.dilate(src, dst, M, anchor, 1, cv.BORDER_CONSTANT, cv.morphologyDefaultBorderValue());
+  // Now that we are finish, we want to write `dst` to file `output.png`. For this we create a `Jimp`
+  // image which accepts the image data as a [`Buffer`](https://nodejs.org/docs/latest-v10.x/api/buffer.html).
+  // `write('output.png')` will write it to disk and Jimp infers the output format from given file name:
+  new jimp({
+    width: dst.cols,
+    height: dst.rows,
+    data: Buffer.from(dst.data)
+  })
+  .write('output.png');
+  src.delete();
+  dst.delete();
+}
+
+// Finally, load the open.js as before. The function `onRuntimeInitialized` contains our program.
+Module = {
+  onRuntimeInitialized
+};
+cv = require('./libs/opencv.js');
